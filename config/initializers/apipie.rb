4008d827363862fe013c3353ee7482374082eb77
Amiruddin Nagri <amir.nagri+git@gmail.com>
2016-06-23 21:32:22 +0530

    [Amir] removing apipie validations

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -4,4 +4,5 @@ Apipie.configure do |config|
   config.doc_base_url            = "/apipie"
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
+  config.validate = false
 end
